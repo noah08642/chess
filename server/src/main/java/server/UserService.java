9120739075980f125d51bb137ede6354f366ba9a
noah08642 <lukerichards8@gmail.com>
2024-10-12 23:50:44 -0600

    Tried to implement UserService

--- server/src/main/java/server/UserService.java
+++ server/src/main/java/server/UserService.java
@@ -1,16 +1,30 @@
 package server;
 
-
+import dataaccess.MemoryUserDAO;
+import dataaccess.UserDAO;
+import request.RegisterRequest;
+import request.LoginRequest;
+import request.LogoutRequest;
 import model.AuthData;
 import model.UserData;
+import result.LoginResult;
 
 public class UserService {
 
-    UserService(Request request) {
+    public LoginResult register(RegisterRequest register) {
+        String user = register.username();
+        String pass = register.password();
+        String mail = register.email();
+
+        UserDAO userDAO = new MemoryUserDAO();
+
 
     }
 
-    public AuthData register(UserData user) {}
-    public AuthData login(UserData user) {}
-    public void logout(AuthData auth) {}
+
+
+    //public AuthData register(UserData user) {}
+    //public AuthData login(UserData user) {}
+    //public void logout(AuthData auth) {}
+
 }
