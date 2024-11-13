22745ec070df757374afe8eea3031a8141005433
noah08642 <lukerichards8@gmail.com>
2024-11-12 21:46:33 -0700

    Moved repl code to ClientMain.java

--- client/src/main/java/ClientMain.java
+++ client/src/main/java/ClientMain.java
@@ -1,12 +1,34 @@
+import ui.Client;
 import ui.Repl;
 
+import java.util.Scanner;
+
 public class ClientMain {
+
     public static void main(String[] args) {
         var serverUrl = "http://localhost:8080";
         if (args.length == 1) {
             serverUrl = args[0];
         }
+        run(serverUrl);
+    }
 
+    public static void run(String serverUrl) {
+        Client client = new Client(serverUrl);
         new Repl(serverUrl).run();
+
+        System.out.println("\uD83D\uDC36 Welcome to the chess menu. Sign in to start.");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        boolean isNotQuit = true;
+        while(isNotQuit) {
+            System.out.print("\n" + ">>> ");
+            System.out.print(client.menu());
+            choice = scanner.nextInt();
+            isNotQuit = client.eval(choice);
+        }
+
+        System.out.println();
     }
 }
